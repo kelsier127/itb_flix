84a0b7f557f8f6dba8c7ab3863371a72b759a976
hugo.alonso.7e5 <hugoalonso343@gmail.com>
2024-05-03 12:00:52 +0200

    paginas principales creadas

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <!--<h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -26,7 +26,15 @@
       <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    </ul>-->
+    <nav>
+
+      <a href="index.html">Index</a> | 
+      <a href="detalle.html">Detalle</a> | 
+      <a href="peliculas.html">Peliculas</a> | 
+      <a href="forms.html">Form</a>
+
+    </nav>
   </div>
 </template>
 
