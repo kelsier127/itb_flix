473a9d9974944208efe9ef8f8a6f79a65345ec31
hugo.alonso.7e5 <hugoalonso343@gmail.com>
2024-05-08 09:37:22 +0200

    conexion con la pelicula por separado

--- src/components/peliculaArticle.vue
+++ src/components/peliculaArticle.vue
@@ -4,7 +4,6 @@
         <b><router-link to='/pelicula'>{{ titol }}</router-link></b><br>
         <img class="posterPelicula" :src=imgUrl >
         <p>Release Date: <b>{{ releaseYear }}</b></p><br>
-        <p>{{ id }}</p>
         
     </article>
 
@@ -12,6 +11,7 @@
 
 
 <script>
+    import axios from "axios"
 
     export default{
         name: 'peliculaArticle',
@@ -20,6 +20,39 @@
             titol: String,
             releaseYear: String,
             id: String
+        },data(){
+            return{
+                peli:[]
+            }
+            
+        },mounted(){
+            axios.get(`http://www.omdbapi.com/?apikey=d148e2fd&i=${this.id}`)
+            .then(response=>{
+                this.info=response.data;
+                if(this.info&&this.info.Search){
+                    this.peli = this.info.Search.map(pelicula=>({
+                        title: pelicula.Title,
+                        releaseYear: pelicula.Year,
+                        ageRate: pelicula.Rated,
+                        releaseDate: pelicula.Released,
+                        runTime: pelicula.Runtime,
+                        genres: pelicula.Genre,
+                        director: pelicula.Director,
+                        writers: pelicula.Writer,
+                        actors: pelicula.Actors,
+                        plot: pelicula.Plot,
+                        awards:pelicula.Awards,
+                        poster:pelicula.Poster,
+                        metaRating:pelicula.Metascore,
+                        imdbRating: pelicula.imdbRating,
+                        boxOffice:pelicula.BoxOffice
+
+                        
+                    }))
+                }
+                
+            })
+            console.log(this.peli)
         }
     }
     
